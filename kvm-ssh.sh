911a692a5008a31291c7087295175e3a506c98ec
Youssef Wadie <youssefwadie14@gmail.com>
2021-10-19 13:17:51 +0200

    Fix connection issues

--- kvm-ssh.sh
+++ kvm-ssh.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if [[ $# -ne 1 ]]; then
-        printf "Expected one domain got %d\n", $#
+        printf "Expected one domain got %d\n" $#
 	exit 1
 else
 	AVALIABLE_MACHINES=$(sudo virsh list --all)
-	IS_AVALIABLE=$(printf "%s", "$AVALIABLE_MACHINES" | awk 'NR>2 {printf "%s\n", $2}' | grep -E "^${1}$")
-	
+	IS_AVALIABLE=$(printf "%s" "$AVALIABLE_MACHINES" | awk -v domain=$1 'NR>2 && $2 == domain {printf "%s\n", $2}')
 	if [[ -n $IS_AVALIABLE ]]; then
-		MACHINE_STATUS=$(printf "%s", "$AVALIABLE_MACHINES" | grep "${1}" | awk '{printf $3$4}')	
+		MACHINE_STATUS=$(printf "%s" "$AVALIABLE_MACHINES" | awk -v domain=$1 '$2 == domain {printf "%s%s\n", $3, $4}')
+		
 		[[ $MACHINE_STATUS != 'running' ]] && sudo virsh start "$1"
 
 		IP=$(sudo virsh domifaddr "$1" | awk 'NR==3 {printf $4}' | cut -d '/' -f 1)
@@ -16,7 +16,7 @@ else
 			printf 'Wating for the machine boot.'
 		fi
 		while [[ -z $IP ]]; do
-			sleep 1
+			sleep 2
 			printf '.'
 			IP=$(sudo virsh domifaddr "$1" | awk 'NR==3 {printf $4}' | cut -d '/' -f 1)
 		done
@@ -30,21 +30,25 @@ else
 		# ssh "$USERNAME@$IP"
 		# or just hardcode it
 		
+		
 		# Asumming you're using the same username as the host machine
-		if ! ssh "$USER@$IP" 2>/dev/null; then	
+		ssh "$USER@$IP" 2>/dev/null
+		CONNECION_STATUS=$?
+		if [[ $CONNECION_STATUS -ne 0 ]]; then
    			printf 'Trying to connect.'
 		fi
-
-		while ! ssh "$USER@$IP" 2>/dev/null ; do
-   			sleep 3
-   			printf '.'
-		done
+		if [[ $CONNECION_STATUS -ne 0 ]]; then
+			while ! ssh "$USER@$IP" 2>/dev/null ; do
+   				sleep 3
+   				printf '.'
+			done
+		fi
 		printf '\n'
 
 	else
 		printf "%s is not avaliable\n" "$1"
 		printf 'Avaliable domains are:\n'
-		printf "%s\n", "$AVALIABLE_MACHINES"
+		printf "%s\n" "$AVALIABLE_MACHINES" | awk 'NR>2{printf "%s\n", $2}'
 	fi
 fi
 
